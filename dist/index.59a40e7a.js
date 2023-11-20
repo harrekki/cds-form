7b485033acfd5404f149236159f5ed93167e6314
David LaRocco <dlarocco77@gmail.com>
2023-11-20 14:51:29 -0700

    add logic to require radio buttons when promo input is empty

--- dist/index.59a40e7a.js
+++ dist/index.59a40e7a.js
@@ -576,11 +576,23 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"8lRBv":[function(require,module,exports) {
 var _bootstrap = require("bootstrap");
+const promoInput = document.getElementById("promo");
 const radioButtons = document.querySelectorAll('input[name="source"]');
 const textareaDiv = document.querySelector(".other-text");
 const otherText = document.getElementById("other-text");
 document.addEventListener("DOMContentLoaded", ()=>{
     textareaDiv.style.display = "none";
+});
+promoInput.addEventListener("input", ()=>{
+    const referral = document.getElementById("referral");
+    const requiredElem = `<span class="required">*</span>`;
+    if (promoInput.value) {
+        radioButtons.forEach((radioButton)=>radioButton.required = true);
+        referral.innerHTML += requiredElem;
+    } else {
+        radioButtons.forEach((radioButton)=>radioButton.required = false);
+        referral.innerHTML = "How did you hear about us?";
+    }
 });
 radioButtons.forEach((radioButton)=>{
     radioButton.addEventListener("click", (event)=>{
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,25 @@
 import 'bootstrap';
 
+const promoInput = document.getElementById('promo');
 const radioButtons = document.querySelectorAll('input[name="source"]');
 const textareaDiv = document.querySelector('.other-text');
 const otherText = document.getElementById('other-text');
 
 document.addEventListener('DOMContentLoaded', () => {
   textareaDiv.style.display = 'none';
+})
+
+promoInput.addEventListener('input', () => {
+    const referral = document.getElementById('referral');
+    const requiredElem = `<span class="required">*</span>`;
+
+    if(promoInput.value) {
+        radioButtons.forEach(radioButton => radioButton.required = true);
+        referral.innerHTML += requiredElem;
+    } else {
+        radioButtons.forEach(radioButton => radioButton.required = false);
+        referral.innerHTML = "How did you hear about us?"
+    }
 })
 
 radioButtons.forEach(radioButton => {
